459791f988e5fb2e14e83425de8483c49cc325f8
davips <dpsabc@gmail.com>
2020-01-16 18:27:24 -0300

    removing excessive phantoms / rellocating toys

--- examples/toy.py
+++ examples/toy.py
@@ -1,5 +1,22 @@
 # Listar *iris*
+from pjdata.data_creation import read_arff
+
+from cururu.persistence import DuplicateEntryException
 from cururu.pickleserver import PickleServer
+
+lst = PickleServer().list_by_name('iris')
+for phantom in lst:
+    print(phantom)
+
+# Armazenar dataset, sem depender do pacote pjml.
+from cururu.pickleserver import PickleServer
+
+print('Storing iris...')
+try:
+    PickleServer().store(read_arff('iris.arff'))
+    print('ok!')
+except DuplicateEntryException:
+    print('Duplicate! Ignored.')
 
 lst = PickleServer().list_by_name('iris')
 for phantom in lst:
