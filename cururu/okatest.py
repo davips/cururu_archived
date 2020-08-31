79448a28cc9e87578172d77fe110be345a3f98e9
davips <dpsabc@gmail.com>
2020-08-31 20:28:17 -0300

    Better tests for OkaServer shorter printing for history

--- cururu/okatest.py
+++ cururu/okatest.py
@@ -1,10 +1,15 @@
 from cururu.okaserver import OkaServer
 from pjdata.content.specialdata import UUIDData
+from pjdata.creation import read_arff
 
-storage = OkaServer(
-    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9"
-    ".eyJpYXQiOjE1OTg5MTEyNDMsIm5iZiI6MTU5ODkxMTI0MywianRpIjoiMWU4YTg3NWItNzQ4YS00NGI1LWE1MGEtNTA3MDFkOTFjMWMxIiwiZXhwIjoxNTk4OTU0NDQzLCJpZGVudGl0eSI6ImRhdmlwcyIsImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyJ9.Z54-YsE7Sq--NqK34QScGl8J0RVfbg-Igj_pfHp7vK0")
-# storage = PickleServer(db="../backend/app/static")  # TODO: history desapareceu!
-d = storage.fetch(UUIDData("QӔťȕǖΨΤǘհЫȬǞшA"))
-print(d.history)
+storage = OkaServer("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1OTg5MTYzNzAsIm5iZiI6MTU5ODkxNjM3MCwianRpIjoiMjU5YmY2YTQtNjZiMy00MGJjLTllOTAtYjQ3MTRlN2NhMmM5IiwiZXhwIjoxNTk4OTU5NTcwLCJpZGVudGl0eSI6ImRhdmlwcyIsImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyJ9.J7tKH2_z0ChIs4SICygNL4nq3-w6FI9GxR_g9aPoe6k")
+
+print("Reading file...")
+data = read_arff("iris.arff")[1]
+
+print("Storing...")
+storage.store(data)
+
+print("Fetching...")
+d = storage.fetch(UUIDData("ĹЇЖȡfĭϹƗͶэգ8Ƀű"))
 print(d)
